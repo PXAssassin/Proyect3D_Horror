92dedba9c9d4df0e979b3904facbc3f358527717
PXAssassin <luis.yanten@uao.edu.co>
2025-05-25 17:16:59 -0500

    2

---
+++ HorrorGame_V1/Assets/Scripts/Inventario/InicializarInventario.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InicializadorInventario : MonoBehaviour
+{
+    public GameObject prefabInventario;
+
+    private static bool instanciado = false;
+
+    void Awake()
+    {
+        if (!instanciado)
+        {
+            GameObject inventario = Instantiate(prefabInventario);
+            DontDestroyOnLoad(inventario);
+            instanciado = true;
+        }
+    }
+}
